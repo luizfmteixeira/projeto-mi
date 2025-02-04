7658701570bb0f0b3dd289e3150bfbfb74961272
luizfmteixeira <medeirosteixeiraluizfilipe@gmail.com>
2025-02-03 22:28:24 -0300

    Lógica contador tempo

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let horaAtual = new Date()
+const horaTeAmo = new Date("May 12, 2019 23:34:00")
+let diferencaMs = horaAtual - horaTeAmo
+
+let segundos = diferencaMs / 1000
+
+let anos = Math.floor(segundos / 60 / 60 / 24 / 30.4 / 12)
+segundos -= (anos * 60 * 60 * 24 * 30.4 * 12)
+let meses = Math.floor(segundos / 60 / 60 / 24 / 30.4)
+segundos -= (meses * 60 * 60 * 24 * 30.4)
+let dias = Math.floor(segundos / 60 / 60 / 24)
+segundos -= (dias * 60 * 60 * 24)
+let horas = Math.floor(segundos / 60 / 60)
+segundos -= (horas * 60 * 60)
+let minutos = Math.floor(segundos / 60)
+segundos -= (minutos * 60)
+segundos = Math.floor(segundos)
+
+console.log(`${anos} ${meses} ${dias} ${horas} ${minutos}:${segundos}`)
